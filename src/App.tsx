285bfdeb1809aedf3ea41c87f708c5b910fbaf75
Sam Bloomingdale <samblooms@users.noreply.github.com>
2022-01-24 17:23:09 -0600

    Added site

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { css } from "@emotion/react"
+import styled from "@emotion/styled/macro"
+
+import { ContentSwitch } from "./components"
+import { Settings } from "./Settings/Settings"
+import { Startpage } from "./Startpage/Startpage"
+
+const Layout = styled.div`
+  ${({ theme: { color } }) => css`
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${color.bg.base};
+  `}
+`
+
+const App = () => (
+  <Layout>
+    <ContentSwitch leftContent={Startpage} rightContent={Settings} />
+  </Layout>
+)
+
+export default App
